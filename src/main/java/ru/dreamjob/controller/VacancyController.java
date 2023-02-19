946d4b405151dd791ff440e6a56db8a07db518e5
Kvardakov <jimbomaxkvr@gmail.com>
2023-02-19 22:51:47 +0300

    Added PostMapping create vacancy

--- src/main/java/ru/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/dreamjob/controller/VacancyController.java
@@ -3,7 +3,10 @@ package ru.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.dreamjob.model.Vacancy;
 import ru.dreamjob.repository.MemoryVacancyRepository;
 import ru.dreamjob.repository.VacancyRepository;
 
@@ -22,6 +25,12 @@ public class VacancyController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 
 }

6f9b3a29e171478e663ed5b6a5fe4f87e32851e6
Kvardakov <jimbomaxkvr@gmail.com>
2023-02-25 22:45:39 +0300

    fix checkstyle

--- src/main/java/ru/dreamjob/repository/MemoryCityRepository.java
+++ src/main/java/ru/dreamjob/repository/MemoryCityRepository.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Repository
-public class MemoryCityRepository implements CityRepository{
+public class MemoryCityRepository implements CityRepository {
 
     private final Map<Integer, City> cities = new HashMap<>() {
         {
--- src/main/java/ru/dreamjob/service/SimpleCityService.java
+++ src/main/java/ru/dreamjob/service/SimpleCityService.java
@@ -3,12 +3,11 @@ package ru.dreamjob.service;
 import org.springframework.stereotype.Service;
 import ru.dreamjob.model.City;
 import ru.dreamjob.repository.CityRepository;
-import ru.dreamjob.repository.MemoryCityRepository;
 
 import java.util.Collection;
 
 @Service
-public class SimpleCityService implements CityService{
+public class SimpleCityService implements CityService {
 
     private final CityRepository repository;
 

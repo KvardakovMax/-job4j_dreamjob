078a222d36ee89a16178e17e593ba4f7c3bc670a
Kvardakov <jimbomaxkvr@gmail.com>
2023-02-25 18:32:27 +0300

    Added vacancy checkbox

--- src/main/java/ru/dreamjob/model/Vacancy.java
+++ src/main/java/ru/dreamjob/model/Vacancy.java
@@ -6,20 +6,24 @@ import java.util.Objects;
 public class Vacancy {
 
     private int id;
+
     private String title;
 
     private String description;
 
     private LocalDateTime creationDate = LocalDateTime.now();
 
+    private boolean visible;
+
     public Vacancy() {
     }
 
-    public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
+    public Vacancy(int id, String title, String description, LocalDateTime creationDate, boolean visible) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.creationDate = creationDate;
+        this.visible = visible;
     }
 
     public int getId() {
@@ -52,6 +56,14 @@ public class Vacancy {
 
     public void setCreationDate(LocalDateTime creationDate) {
         this.creationDate = creationDate;
+    }
+
+    public boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override

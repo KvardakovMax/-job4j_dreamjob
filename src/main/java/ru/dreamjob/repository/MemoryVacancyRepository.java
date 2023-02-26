45cea07c7e444692b5b2096118fd648388ba9924
Kvardakov <jimbomaxkvr@gmail.com>
2023-02-26 11:40:52 +0300

    fix checkstyle

--- src/main/java/ru/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/dreamjob/repository/MemoryVacancyRepository.java
@@ -17,12 +17,24 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new HashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "An intern is typically temporary and often receives school credits for their work.", LocalDateTime.now(), false, 0));
-        save(new Vacancy(0, "Junior Java Developer", "This is a beginner specialist who already knows the base", LocalDateTime.now(), false, 0));
-        save(new Vacancy(0, "Junior+ Java Developer", "Strong junior developer", LocalDateTime.now(), false, 0));
-        save(new Vacancy(0, "Middle Java Developer", "Mid-level seniority involves having a managerial position over entry-level employees while also reporting to someone with more seniority", LocalDateTime.now(), false, 0));
-        save(new Vacancy(0, "Middle+ Java Developer", "More advanced mid-level employees are sometimes described as \"mid-senior\" to indicate their seniority over other middle management positions.", LocalDateTime.now(), false, 0));
-        save(new Vacancy(0, "Senior Java Developer", "Senior developers may specialize in a specific area, oversee projects, and manage junior developers.", LocalDateTime.now(), false, 0));
+        save(new Vacancy(0, "Intern Java Developer",
+                "An intern is typically temporary and often receives school credits for their work.",
+                LocalDateTime.now(), false, 0));
+        save(new Vacancy(0, "Junior Java Developer",
+                "This is a beginner specialist who already knows the base",
+                LocalDateTime.now(), false, 0));
+        save(new Vacancy(0, "Junior+ Java Developer",
+                "Strong junior developer",
+                LocalDateTime.now(), false, 0));
+        save(new Vacancy(0, "Middle Java Developer",
+                "Mid-level seniority involves having a managerial position over entry-level employees while also reporting to someone with more seniority",
+                LocalDateTime.now(), false, 0));
+        save(new Vacancy(0, "Middle+ Java Developer",
+                "More advanced mid-level employees are sometimes described as \"mid-senior\" to indicate their seniority over other middle management positions.",
+                LocalDateTime.now(), false, 0));
+        save(new Vacancy(0, "Senior Java Developer",
+                "Senior developers may specialize in a specific area, oversee projects, and manage junior developers.",
+                LocalDateTime.now(), false, 0));
     }
 
     @Override

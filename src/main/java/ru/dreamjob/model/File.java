ecbbee34aeda5ebe144e20519eb6a6087e48b084
Kvardakov <jimbomaxkvr@gmail.com>
2023-02-26 23:05:58 +0300

    Fixed checkstyle autogenerated methods

--- src/main/java/ru/dreamjob/model/File.java
+++ src/main/java/ru/dreamjob/model/File.java
@@ -41,8 +41,12 @@ public class File {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         File file = (File) o;
         return id == file.id && Objects.equals(path, file.path);
     }

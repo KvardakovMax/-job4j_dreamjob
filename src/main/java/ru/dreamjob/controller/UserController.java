2d254e7cbe14d820f31214fdf2134c614610fe83
Kvardakov <jimbomaxkvr@gmail.com>
2023-03-04 19:14:45 +0300

    Fixed registration page view

--- src/main/java/ru/dreamjob/controller/UserController.java
+++ src/main/java/ru/dreamjob/controller/UserController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.sql2o.Sql2oException;
 import ru.dreamjob.model.User;
+import ru.dreamjob.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/register")
     public String getRegistrationPage() {
@@ -19,6 +27,27 @@ public class UserController {
 
     @PostMapping("/register")
     public String register(Model model, @ModelAttribute User user) {
-        return "";
+        try {
+            userService.save(user);
+        } catch (Sql2oException e) {
+            model.addAttribute("error", "This user already exist");
+            return "users/register";
+        }
+        return "redirect:/index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(Model model, @ModelAttribute User user) {
+        var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Wrong email or password");
+            return "users/login";
+        }
+        return "redirect:/vacancies";
     }
 }
